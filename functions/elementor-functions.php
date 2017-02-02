a3188cfcd8b32423306c8e48640409ae405dee3b
Zulfikar Nore <git@wpdevhq.com>
2017-02-02 14:39:12 +0000

    Removed CSS ID
    CSS ID removed as it is now part of Elementor Core

--- functions/elementor-functions.php
+++ functions/elementor-functions.php
@@ -15,37 +15,4 @@ function elementor_accordion_title() { ?>
 <?php }
 add_action( 'wp_footer', 'elementor_accordion_title', 99 );
 
-// Adds the ability to add CSS ID to an Elementor section :)
-add_action('elementor/element/section/section_advanced/before_section_end', function ($instance, $section) {
-	$instance->add_control(
-		'universal_section_id',
-		[
-			'type' => \Elementor\Controls_Manager::TEXT,
-			'label' => __('CSS ID eg. #my-id', 'wp-universal-functions'),
-		]
-	);
-}, 10, 2);
- 
-add_action('elementor/frontend/element/before_render', function ($instance) {
-	if ('section' == $instance->get_name()) {
-		ob_start();
-	}
-});
-
-add_action('elementor/frontend/element/after_render', function ($instance) {
-	if ('section' == $instance->get_name()) {
-		$content = ob_get_clean();
-		$sectionID = $instance->get_settings('universal_section_id');
-		if (!empty($sectionID)) {
-			preg_match('/<section (.*?)(>)/', $content, $sectionTag);
-
-			if (!empty($sectionTag) && !preg_match('/id="(.*?)/', $sectionTag[0])) {
-				$replacedSectionID = sprintf('<section id="%s"', esc_attr($sectionID));
-				$replaced = str_replace('<section', $replacedSectionID, $sectionTag[0]);
-
-				$content = str_replace($sectionTag[0], $replaced, $content);
-			}
-		}
-		echo $content;
-	}
-});
+// CSS ID removed as this is now part of Elementor Core
